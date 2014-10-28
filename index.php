ac26e45bb0557e40fe84e0393b87990066e65962
smeisinger <hello@simonmeisinger.at>
2014-10-28 10:32:17 +0100

    ha?
    Dateien hinzugefügt

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Wohnen in Kumberg </title>
+	<meta name="description" content="Traumimmobilien 10 Minuten von Graz, direkt im Grünen!" />
+	<meta name="ROBOTS" content="ALL" />
+
+	<link href="style.css" rel="stylesheet" media="screen">
+	<link href="MyFontsWebfontsKit.css" rel="stylesheet" media="screen">
+	
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+</head>
+
+<body>
+<a id="top"></a>
+
+<div id="container">
+
+<div id="logo">
+<img src="img/logo.png" alt="wohnen in kumberg logo" title="Wohnen In Kumberg">
+</div>
+
+
+<div id="topnav">
+<a href="#projekt">Projekt</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#var">Hausvarianten</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#infos">Infos</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#lage">Lage & Umfeld</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#kontakt">Anfrage & Kontakt</a>
+</div>
+
+<div id="sticky-anchor" style="display:none; position:fixed;">
+<a href="#top"><img style="position:absolute; top:0px; left: 0px;" src="http://simonmeisinger.at/temp/wik/img/topnav_background.gif" alt="logo"></a>
+<a href="#projekt">Projekt</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#var">Hausvarianten</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#infos">Infos</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#lage">Lage & Umfeld</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#kontakt">Anfrage & Kontakt</a>
+</div>
+
+<div id="topimg">
+<div id="cycler">
+<img class="active" src="img/topimg.jpg" alt="Rendering 1" />
+<img src="img/topimg2.jpg" alt="Rendering 2" />	
+<img src="img/topimg3.jpg" alt="Rendering 3" />	
+</div>
+</div>
+
+
+<div id="content">
+
+
+<div class="row firstrow">
+<div class="leftcollumn">
+<h1>„10 Minuten von Graz, direkt im Grünen!“</h1>
+</div>
+
+<div class="rightcollumn">
+<h2>Ihre Traumimmobilie<br/>in absoluter Ruhelage!</h1>
+<p class="twocollumn">
+Mit Aussicht nach Kumberg entstehen 8 Atrium Doppelhäuser die durch moderne offene Planung ein ganz besonderes Ambiente bieten. Die 100m2 Lebensraum sind auf zwei Ebenen aufgeteilt, werden durch Terrassen und Ausblicke aufgelockert und geben Luft zum Atmen. Und das Beste daran, sie können aus zwei Varianten wählen und die Ausstattung frei bestimmen. 10 Minuten von Graz entfernt und doch in absoluter Ruhelage kann man sein Leben genießen.
+</p>
+</div>
+</div> <!--row-->
+
+
+<div class="row">
+<div class="leftcollumn stripes">
+<h3>
+<span id="projekt"> &nbsp; </span>
+Projekt</h3>
+</div>
+
+<div class="rightcollumn">
+<h2>8 Atrium Doppelhäuser</h2>
+<ul class="facts factsleft">
+<li>Toplage in Kumberg, 10 min. von Mariatrost</li>
+<li>Offene Raumaufteilung</li>
+<li>Beeindruckende Raumhöhen</li>
+<li>Im Inneren ist individuelle Planung möglich</li>
+<li>Großzügiges Atrium und Balkone</li>
+<li>Direktes und indirektes Licht</li>
+</ul>
+<ul class="facts">
+<li>Hochwertige Ausstattung</li>
+<li>Raumhohe Fenster- und Portalelemente</li>
+<li>Faszinierende Licht- und Blickerlebnisse</li>
+<li>Zentrales Heizsystem</li>
+<li>Nach dem letzten Stand der Technik</li>
+<li>Hauseigenes Carport</li>
+</ul>
+</div>
+</div> <!--row-->
+
+
+<div class="row">
+<div class="leftcollumn var1">
+<h3>
+<span id="var"> &nbsp; </span>
+VAR1</h3>
+</div>
+
+<div class="rightcollumn">
+<h2>Atrium Doppelhäuser</h2>
+<ul class="facts">
+<li>Schlaf- und Kinderzimmer im EG</li>
+<li>Wohn-, Ess- und Küchenbereich im OG</li>
+<li>extra hohe Raumhöhen im Wohnbereich</li>
+</ul>
+<a class="download" href="#">
+Pläne, Schnitte, Ansichten<br />
+Download PDF
+</a>
+</div>
+</div> <!--row-->
+
+
+<div class="row">
+<div class="leftcollumn var2">
+<h3>VAR2</h3>
+</div>
+
+<div class="rightcollumn">
+<h2>8 Atrium Doppelhäuser</h2>
+<ul class="facts">
+<li>Schlaf- und Kinderzimmer im OG</li>
+<li>Wohn-, Ess- und Küchenbereich im EG</li>
+</ul>
+<a class="download" href="#">
+Pläne, Schnitte, Ansichten<br />
+Download PDF
+</a>
+</div>
+</div> <!--row-->
+
+
+<div class="row">
+<div class="leftcollumn stripes">
+<h3>
+<span id="infos"> &nbsp; </span>
+Infos</h3>
+</div>
+
+<div class="rightcollumn">
+<h2>Interessantes</h2>
+<ul class="facts">
+<li>Kein Bauträgerprojekt</li>
+<li>Innenräume - individuelle Planung möglich</li>
+<li>Sie können die Kosten mitbestimmen</li>
+<li>Ausstattung/Materialien frei wählbar</li>
+<li>Ein Heizsystem für alle Einheiten</li>
+<li>Fassade offen oder geschlossen wählbar</li>
+</ul>
+</div>
+</div> <!--row-->
+
+
+<div class="row">
+<div class="leftcollumn stripes">
+<h3>
+<span id="lage"> &nbsp; </span>
+Lage & Umfeld</h3>
+</div>
+
+<div class="rightcollumn">
+<div class="rightcollumn_small">
+<h2>Fasslberg</h2>
+<ul class="facts">
+<li>Prellerbergstraße</li>
+<li>Toplage nähe Kumberg</li>
+<li>10 min. von Mariatrost</li>
+<li>Öffentliche Verkehrsmittel in der Nähe</li>
+<li>Ausblick nach Kumberg</li>
+</ul>
+</div>
+<div class="rightcollumn_small">
+<iframe src="http://www.map-generator.org/2b4434d7-e08c-48e9-9833-7aa838a2c7ce/iframe-map.aspx" scrolling="no" height="175px" width="325px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br><small><a href="http://www.map-generator.org/2b4434d7-e08c-48e9-9833-7aa838a2c7ce/large-map.aspx" target="_blank">Auf Google Maps öffnen<a/></small>
+</div>
+</div>
+</div> <!--row-->
+
+</div> <!--content-->
+
+
+<div id="contentgrey">
+
+<div class="row firstrow">
+<div class="leftcollumn whitestripes">
+<h3 class="grey">
+<span id="kontakt"> &nbsp; </span>
+Anfrage &<br />Kontakt
+</h3>
+</div>
+
+<div class="rightcollumn">
+<h2>Infoabend</h2>
+<p class="rightcollumn_small">Datum und Ort wird so bald wie möglich bekannt gegeben.<br /><br /><br />
+<strong>Kontakt Planung<br />
+Steidl Architekten</strong><br />
+Stockergasse 10 | 8020 Graz<br />
+<a href="mailto:office@steidl-architekten.com">office@steidl-architekten.com</a><br />
+<a href="http://www.st-ar.com">www.st-ar.com</a></p>
+
+<p class="rightcollumn_small">
+<a class="email" href="office@wohneninkumberg.com">
+Bitte schreiben Sie uns ein Email.<br />
+Jede Anfrage wird so schnell wie<br />
+möglich bearbeitet.</a><br /><br /><br /><br />
+<strong>Kontakt Projektmanagement<br />
+ZT Kanzlei DI Robert Perci</strong><br />
+Dietrichsteinplatz 15/6 | A-8010 Graz<br />
+T: 0316 83 14 26-13<br />
+M: 0664 840 71 29<br />
+<a href="mailto:office@zt-perci.at">office@zt-perci.at</a> | <a href="http://www.zt-perci.at">www.zt-perci.at</a>
+</p>
+</div>
+
+</div> <!--row-->
+
+</div> <!--contentgrey-->
+
+
+
+<div id ="content">
+
+<div class="row firstrow" style="margin-bottom:50px;">
+<div class="leftcollumn stripes">
+<h3>
+<span> &nbsp; </span>
+Impressum</h3>
+</div>
+
+<div class="rightcollumn">
+<strong>ZT Kanzlei DI Robert Perci</strong><br />
+Dietrichsteinplatz 15/6 | A-8010 Graz<br />
+T: 0316 83 14 26-13<br />
+M: 0664 840 71 29<br />
+<a href="mailto:office@zt-perci.at">office@zt-perci.at</a> | <a href="http://www.zt-perci.at">www.zt-perci.at</a>
+</div>
+</div> <!--row-->
+
+</div> <!--content-->
+
+
+
+</div> <!--container-->
+
+<script type="text/javascript">
+$(document).ready(function() {
+  function filterPath(string) {
+  return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+  }
+  var locationPath = filterPath(location.pathname);
+  var scrollElem = scrollableElement('html', 'body');
+ 
+  $('a[href*=#]').each(function() {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    if (  locationPath == thisPath
+    && (location.hostname == this.hostname || !this.hostname)
+    && this.hash.replace(/#/,'') ) {
+      var $target = $(this.hash), target = this.hash;
+      if (target) {
+        var targetOffset = $target.offset().top;
+        $(this).click(function(event) {
+          event.preventDefault();
+          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+            location.hash = target;
+          });
+        });
+      }
+    }
+  });
+ 
+  // use the first element that is "scrollable"
+  function scrollableElement(els) {
+    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+      var el = arguments[i],
+          $scrollElement = $(el);
+      if ($scrollElement.scrollTop()> 0) {
+        return el;
+      } else {
+        $scrollElement.scrollTop(1);
+        var isScrollable = $scrollElement.scrollTop()> 0;
+        $scrollElement.scrollTop(0);
+        if (isScrollable) {
+          return el;
+        }
+      }
+    }
+    return [];
+  }
+ 
+});
+</script>
+
+<script type="text/javascript">
+$(function(){
+    $(window).scroll(function() {              
+	    $(document).scrollTop() > 450 ? $('#sticky-anchor:hidden').fadeIn() : $('#sticky-anchor:visible').fadeOut();    });
+})
+</script>
+
+
+<script type="text/javascript">
+function cycleImages(){
+      var $active = $('#cycler .active');
+      var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+      $next.css('z-index',2);//move the next image up the pile
+      $active.fadeOut(1500,function(){//fade out the top image
+	  $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+          $next.css('z-index',3).addClass('active');//make the next image the top one
+      });
+    }
+
+$(document).ready(function(){
+// run every 7s
+setInterval('cycleImages()', 7000);
+})
+</script>
+
+
+</body>
+</html>
